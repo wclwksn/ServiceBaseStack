fb6486c5e9f4d3fe58c03e7c2f6b0cb9805fd819
wclwksn <wclwksn@sina.com>
2016-07-25 17:47:46 +0800

    get user info

---
+++ Base.ST/Base.ST.ServiceInterface/UserService.cs
@@ -0,0 +1,28 @@
+﻿using Base.ST.DomainModel;
+using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.OrmLite;
+
+namespace Base.ST.ServiceInterface
+{
+    public class UserService : Service
+    {
+        public object Get(getUserInfoByName userInfo)
+        {
+            sms_user _smsUser = new sms_user(); 
+            _smsUser = Db.Single<sms_user>(x => x.name == userInfo.name);
+            _smsUser.password = string.Empty;
+            return _smsUser;
+        }
+    }
+    [Authenticate]
+    [Route("/SmsUser/{name}", "GET")]
+    public class getUserInfoByName : IReturn<sms_user>
+    {
+        public string name { get; set; }
+    }
+}
